fa5cb2d4dcb8dbe9ded262d6b267389cb5c67c9d
Kyle Raney <raney24fcb@gmail.com>
2015-01-21 19:35:51 -0500

    added test class

---
+++ KRTest.h
@@ -0,0 +1,13 @@
+//
+//  KRTest.h
+//  Git Test
+//
+//  Created by Kyle Raney on 1/21/15.
+//  Copyright (c) 2015 Kyle Raney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KRTest : NSObject
+
+@end
---
+++ KRTest.m
@@ -0,0 +1,13 @@
+//
+//  KRTest.m
+//  Git Test
+//
+//  Created by Kyle Raney on 1/21/15.
+//  Copyright (c) 2015 Kyle Raney. All rights reserved.
+//
+
+#import "KRTest.h"
+
+@implementation KRTest
+
+@end
